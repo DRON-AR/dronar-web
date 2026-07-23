1f3a42f35a7f76f3ef1e3258f814955f8af4737e
Daniel Campo Pérez <danielcampo88@gmail.com>
2026-07-23 17:42:53 -0500

    Update backend entrypoint to vercel.ts

---
+++ backend/src/vercel.ts
@@ -0,0 +1,11 @@
+﻿import fastify from 'fastify';
+import server from './server';
+
+const app = fastify();
+
+app.register(server);
+
+export default async function handler(req, res) {
+  await app.ready();
+  app.server.emit('request', req, res);
+}
